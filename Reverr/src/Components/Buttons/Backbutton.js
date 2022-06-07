050d9ec27287f6e69abf818d0c89f64fd22b0365
Bhavya Kohli <kohlibhavya18@gmail.com>
2022-06-07 18:45:06 +0530

    feat:firebase login functionality

---
+++ Reverr/src/Components/Buttons/Backbutton.js
@@ -0,0 +1,34 @@
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { AppColors } from '../../utils/Constants';
+
+const BackButton = props => {
+  return (
+    <View>
+      <TouchableOpacity
+        style={{...styles.btn, ...props.style}}
+        onPress={props.onPress}>
+        <Icon
+          name="angle-left"
+          size={props.IconSize}
+          color={AppColors.FontsColor}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: AppColors.CardColor,
+    borderRadius: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginStart: 10,
+    width: 30,
+    height: 30,
+  },
+});
+
+export default BackButton;
---
+++ Reverr/src/Components/InputField.js
@@ -0,0 +1,86 @@
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+    Dimensions,
+  } from 'react-native';
+  import React from 'react';
+  import { AppColors } from '../utils/Constants';
+  import Icon from 'react-native-vector-icons/FontAwesome';
+  const Height = Dimensions.get('window').height;
+  const Width = Dimensions.get('window').width;
+  
+  const InputField = props => {
+    return (
+      <View style={styles.screen}>
+        <View style={{...styles.box, ...props.style}}>
+          <Icon
+            name={props.iconName}
+            size={props.size}
+            color={'white'}
+            style={{marginStart: 15}}
+          />
+          <View style={{marginStart: '3%'}}>
+            <Text
+              style={[
+                styles.heading,
+                {color: props.error ? 'red' : AppColors.FontsColor},
+              ]}>
+              {props.Title}
+            </Text>
+            <TextInput
+              style={styles.input}
+              placeholder={props.placeholder}
+              placeholderTextColor={AppColors.infoFonts}
+              onChangeText={props.onChangeText}
+              secureTextEntry={props.secureTextEntry}
+              keyboardType={props.keyboardType}
+              value={props.value}
+              maxLength={props.maxLength}
+            />
+          </View>
+          <TouchableOpacity onPress={props.Eyelick}>
+            <Icon
+              name={props.showIcon}
+              size={props.showIconsize}
+              color={props.showIconolor}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+  const styles = StyleSheet.create({
+    screen: {
+      alignItems: 'center',
+      marginBottom: Height / 32,
+    },
+    box: {
+      backgroundColor: AppColors.inputFieldColor,
+      width: '99%',
+      paddingVertical: '2%',
+      borderRadius: 10,
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    heading: {
+      color: AppColors.FontsColor,
+      marginStart: '2%',
+      fontFamily: 'Poppins-Regular',
+    },
+    input: {
+      fontSize: 13,
+      color: AppColors.FontsColor,
+      paddingTop: 0,
+      fontFamily: 'Poppins-Regular',
+      marginTop: -7,
+      paddingBottom: 0,
+      height: Height / 16,
+      width: Width / 1.5,
+    },
+  });
+  
+  export default InputField;
+  
