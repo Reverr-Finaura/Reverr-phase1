c9c9ffd07b548bba4cd2905e55315876e12b81c0
dharmendra841434 <dhk7283013841@gmail.com>
2022-10-09 14:00:09 +0530

    changes done

--- src/scenes/ProfileScreens/IndivisualProfile/index.js
+++ src/scenes/ProfileScreens/IndivisualProfile/index.js
@@ -7,31 +7,19 @@ import {
   TouchableOpacity,
   Dimensions,
   ActivityIndicator,
-  FlatList,
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import {AppColors} from '../../../utils';
-import {BackButton, CustomPopup, ProfileDetailsBox} from '../../../Components';
+import {ProfileDetailsBox} from '../../../Components';
 import Icon2 from 'react-native-vector-icons/FontAwesome5';
-import Ionic from 'react-native-vector-icons/Ionicons';
 import {useNavigation} from '@react-navigation/native';
 import {useDispatch, useSelector} from 'react-redux';
 import {styles} from '../viewProfile/style';
-import auth from '@react-native-firebase/auth';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Icon3 from 'react-native-vector-icons/Ionicons';
 import LinearGradient from 'react-native-linear-gradient';
-import {getPost} from '../../../utils/FirebaseFunctionality';
-import {styles2} from '../../Room/styles';
-import {
-  load_room_data,
-  refresh_rooms_list,
-  set_allLoaded,
-  like_post,
-  pin_post,
-  deletePost,
-} from '../../../Redux/actions';
+import {load_room_data, set_allLoaded} from '../../../Redux/actions';
 import {mentorService} from '../../../Redux/services/mentor.service';
+import PostCard from '../../../Components/postCard';
 
 const Width = Dimensions.get('screen').width;
 const Height = Dimensions.get('screen').height;
@@ -41,217 +29,11 @@ const IndividuaProfile = props => {
   const state = useSelector(state => state.UserReducer);
   const [posts, setPosts] = useState(false);
   const [about, setAbout] = useState(true);
-  const [popup, setPopup] = useState(false);
   const [_id, set_Id] = useState('');
-  const [owner, setOwner] = useState(false);
   const dispatch = useDispatch();
   const [postData, setPostData] = useState('');
 
-  const handleDelete = post => {
-    console.log(post.id, 'postdetails');
-    dispatch(deletePost(post, post.id));
-  };
-  const likePost = (id, item) => {
-    dispatch(like_post(id, item, state.user.email));
-  };
-
-  const _handleLoadMore = () => {
-    console.log('on end reached dispatched');
-    dispatch(load_room_data(state?.lastDocument || undefined));
-  };
-
-  const handleRefresh = () => {
-    dispatch(set_allLoaded(false));
-    dispatch(refresh_rooms_list());
-  };
-
   // console.log(state.Rooms.postedby === state.user.email, 'RoomsData');
-
-  const renderCard = ({item, index}) => {
-    console.log(state.allLoaded, 'Hey!');
-    // console.log(state.Rooms[0], 'userdta');
-    return (
-      <LinearGradient
-        key={item.id}
-        colors={[AppColors.CardColor, AppColors.CardColor]}
-        start={{x: -3, y: 1.3}}
-        end={{x: 3, y: 0.5}}
-        style={styles2.postCard}>
-        <View style={styles2.creatorDetails}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate('ViewProfile', {
-                  postData: item,
-                });
-              }}>
-              <Image style={styles2.dp} source={{uri: item.postedby.image}} />
-            </TouchableOpacity>
-            <View style={{marginStart: '3%'}}>
-              <Text style={styles2.name}>{item.postedby.name}</Text>
-              <Text style={styles2.company}>@{item.postedby.name}</Text>
-            </View>
-          </View>
-          <View style={{flexDirection: 'row'}}>
-            <Text
-              style={{
-                color: AppColors.BtnClr,
-                fontWeight: 'bold',
-                alignSelf: 'center',
-                marginTop: '-5%',
-                marginRight: '4%',
-              }}>
-              12/08/2022
-            </Text>
-            <TouchableOpacity
-              onPress={() => {
-                setPopup(true);
-                set_Id(item.id);
-                setOwner(
-                  state.user.email == item.postedby.email ? true : false,
-                );
-                // alert(item.id);
-              }}>
-              <Icon3
-                name="ellipsis-vertical"
-                size={22}
-                color={AppColors.ActiveColor}
-              />
-            </TouchableOpacity>
-          </View>
-        </View>
-        {item.id === _id && (
-          <CustomPopup
-            modalVisible={popup}
-            setModalVisible={setPopup}
-            onRequestClose={() => setPopup(false)}>
-            <View>
-              {owner && (
-                <View
-                  style={{
-                    borderBottomColor: AppColors.FontsColor,
-                    borderBottomWidth: 1,
-                    paddingVertical: '4%',
-                    alignItems: 'center',
-                  }}>
-                  <TouchableOpacity
-                    onPress={() => {
-                      handleDelete(item);
-                      // alert(item.id);
-                      setPopup(false);
-                    }}>
-                    <Text style={{color: AppColors.FontsColor}}>Delete</Text>
-                  </TouchableOpacity>
-                </View>
-              )}
-              <View
-                style={{
-                  borderBottomColor: AppColors.FontsColor,
-                  borderBottomWidth: 1,
-                  alignItems: 'center',
-                  paddingVertical: '4%',
-                }}>
-                <TouchableOpacity>
-                  <Text style={{color: AppColors.FontsColor}}>Share</Text>
-                </TouchableOpacity>
-              </View>
-              <View
-                style={{
-                  borderBottomColor: AppColors.FontsColor,
-                  borderBottomWidth: 1,
-                  alignItems: 'center',
-                  paddingVertical: '4%',
-                }}>
-                <TouchableOpacity onPress={() => savePost(item)}>
-                  <Text style={{color: AppColors.FontsColor}}>
-                    {state.savedPosts && state.savedPosts.includes(item.id)
-                      ? 'Unsave'
-                      : 'Save'}
-                  </Text>
-                </TouchableOpacity>
-              </View>
-              <View
-                style={{
-                  paddingVertical: '4%',
-                  alignItems: 'center',
-                }}>
-                <TouchableOpacity onPress={() => setPopup(false)}>
-                  <Text style={{color: AppColors.FontsColor}}>close</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </CustomPopup>
-        )}
-        <View>
-          {item.image == '' ? (
-            <View style={{paddingHorizontal: '5%'}}>
-              <Text style={styles2.details}>{item.text}</Text>
-            </View>
-          ) : (
-            <View>
-              <Text style={styles2.details}>{item.text}</Text>
-              <View
-                style={[styles2.image, {overflow: 'hidden', marginTop: '2%'}]}>
-                <Image
-                  style={{width: '100%', height: '100%'}}
-                  source={{uri: item.image}}
-                />
-              </View>
-            </View>
-          )}
-        </View>
-        <View style={styles2.IconContainer}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <TouchableOpacity onPress={() => likePost(item.id, item)}>
-              {item.likes.includes(state.user.email) ? (
-                <Image
-                  source={require('../../../assets/images/likedone.png')}
-                  style={{
-                    tintColor: AppColors.ActiveColor,
-                    width: 30,
-                    height: 30,
-                  }}
-                />
-              ) : (
-                <Image
-                  source={require('../../../assets/images/like.png')}
-                  style={{
-                    tintColor: AppColors.ActiveColor,
-                    width: 20,
-                    height: 22,
-                  }}
-                />
-              )}
-            </TouchableOpacity>
-            <Text style={{marginStart: '8%', color: AppColors.BtnClr}}>
-              {item.likes.length} reactions
-            </Text>
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <TouchableOpacity
-              onPress={() => {
-                dispatch(pin_post(item));
-                navigation.navigate('comments', {
-                  postData: item,
-                });
-              }}>
-              <Image
-                source={require('../../../assets/images/comment.png')}
-                style={{
-                  tintColor: AppColors.ActiveColor,
-                  height: 22,
-                  width: 20,
-                }}
-              />
-            </TouchableOpacity>
-            <Text style={{marginStart: '8%', color: AppColors.BtnClr}}>
-              {item.comments.length} comments
-            </Text>
-          </View>
-        </View>
-      </LinearGradient>
-    );
-  };
 
   useEffect(() => {
     dispatch(mentorService);
@@ -261,18 +43,6 @@ const IndividuaProfile = props => {
     } else {
       dispatch(load_room_data(state.lastDocument));
     }
-    /* getPost().then(res => {
-      let dd = res.map(d => d._data);
-      setPostData(
-        dd.filter(
-          item => item.postedby._documentPath._parts[1] == state.user.email,
-        ),
-      );
-      /* console.log(
-        dd[2].postedby._documentPath._parts[1] == state.user.email,
-        'pspsp',
-      ); 
-    }); */
     let t = state.Rooms.filter(item => item.postedby.email == state.user.email);
     setPostData(t);
     console.log(state.Rooms, 'filtered');
@@ -495,19 +265,7 @@ const IndividuaProfile = props => {
             </Text>
           </TouchableOpacity>
         </View>
-        {posts && (
-          <View style={{paddingBottom: '5%'}}>
-            <FlatList
-              data={postData}
-              keyExtractor={item => item.id}
-              renderItem={renderCard}
-              // onEndReached={_handleLoadMore}
-              onEndReachedThreshold={1}
-              // refreshing={state.refreshing}
-              // onRefresh={handleRefresh}
-            />
-          </View>
-        )}
+        {posts && <PostCard postData={postData} />}
         {about && (
           <View>
             <LinearGradient
