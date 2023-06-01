c690dad58c9df2d6c26d3920ae682bc1efc7477e
apurbar06 <apurbar06@gmail.com>
2023-06-01 22:24:39 +0530

    done module 1 of Business Planning

---
+++ src/dumy-Data/courseDataBusinessPlanning.js
@@ -0,0 +1,193 @@
+export const courseDataBusinessPlanning = {
+  frontscreen: {
+    images: [
+      'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%2C%20analytics%2C%20sucess%2C%20startup%2C%20Technology%20_%20account%2C%20setting%2C%20options%2C%20preferences.png?alt=media&token=d017d4cc-0626-488c-8b25-2a478282e65b',
+      'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FFinances.png?alt=media&token=33a14e0c-7a36-4212-add7-bc57254b2aa9',
+    ],
+    name: 'BUSINESS PLANNING',
+  },
+  modules: [
+   {
+    name:"model1",
+    slides:[
+    {
+      heading: 'What is Business Planning?',
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FFinances.png?alt=media&token=33a14e0c-7a36-4212-add7-bc57254b2aa9',
+      type: 'intro',
+    },
+    {
+      text: "“An idea without a plan is just an idea”",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FFinances.png?alt=media&token=33a14e0c-7a36-4212-add7-bc57254b2aa9',
+      type: 'textimg',
+    },
+    {
+      text: "You might have already heard about the importance of a well-constructed business plan.\n\nBut you might not know what exactly a business plan is, what are its key elements, what things you should include and make a clear, well-written plan that can attract people towards your business and get them involved. We are here to help.",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'textimg',
+    },
+    {
+      text: "We are here to help.",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%2C%20analytics%2C%20sucess%2C%20startup%2C%20Technology%20_%20account%2C%20setting%2C%20options%2C%20preferences.png?alt=media&token=d017d4cc-0626-488c-8b25-2a478282e65b',
+      type: 'textimg',
+    },
+    {
+      heading: 'What is actually a business plan?',
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FFinances.png?alt=media&token=33a14e0c-7a36-4212-add7-bc57254b2aa9',
+      type: 'intro',
+    },
+    {
+      text: "Business Plan is a document that helps Investors and readers understand your business goals and the steps you’re taking to reach them.",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'textimg',
+    },
+    {
+      text: "An “actionable plan” requires a bit of work. The best business plans are not just informative; they also excite and persuade readers about taking part (and benefiting from it) in helping your business succeed.",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'textimg',
+    },
+    {
+      text: "How do you do this?",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'textimg',
+    },
+    {
+      text: "You do this by answering the major, fundamental questions that your readers will have about your business going forward.\n\nThese questions fall into two key categories: the WHY questions and the HOW questions.",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'textimg',
+    },
+    {
+      question: 'What is business planning?',
+      options: [
+        'Business Plan is a document that helps employees understand your business goals and the steps you’re taking to reach them.',
+        'Business Plan is a document that helps you understand your business goals and the steps you’re taking to reach them.',
+        'Business Plan is a document that helps Investors and readers understand your business goals and the steps you’re taking to reach them.',
+        'Business Plan is a document that helps Investors and readers understand your business model you’re taking to reach them.',
+      ],
+      ans: 2,
+      type: 'quiz',
+    },
+    {
+      text: "The WHY Questions:\n\n- Why this?\n- Why now?\n- Why you?",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'textimg',
+    },
+    {
+      text: "The HOW Questions:\n\n- How will you make money?\n- How will you get customers?\n- How will you grow your business?",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'textimg',
+    },
+    {
+      text: "And while answering these questions your business plan should also highlight how your company has the right product and the right market.",
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'textimg',
+    },
+    {
+      heading: 'Why do you need a business plan?',
+      text: "",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      heading: '1. To Develop your strategies',
+      text: "Writing down your goals and analyzing market helps you discover new trends in the industry that could help you develop your strategies.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      heading: '2. To guide you',
+      text: "A business plan gives you direction and helps you sort out your ideas, so you can set your goals, prioritize them and create a strategy to reach them.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      heading: '3. To attract Investors',
+      text: "A potential investor wants proof that their time and money will be repaid when they invest in your business. Your business plan helps you make that case.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      question: 'Which of the following is NOT one of the reason you need business plan',
+      options: [
+        'To guide you',
+        'To develop strategies',
+        'To attract customers',
+        'To form strategic partnerships',
+      ],
+      ans: 3,
+      type: 'quiz',
+    },
+    {
+      heading: 'Who Needs a Business Plan?',
+      image:'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FFinances.png?alt=media&token=33a14e0c-7a36-4212-add7-bc57254b2aa9',
+      type: 'intro',
+    },
+    {
+      heading: '1. Business Seeking funding',
+      text: "If you aim to raise money to support or grow your business. Having a business plan is a necessity. Having a business plan increases the likelihood of obtaining funding.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      heading: '2. Established Companies Managing Their Businesses',
+      text: "Unlike startups, existing businesses make extensive use of business plans with a view to directing business and accelerating and tracking growth. Established businesses also use business plans to convince consumers to find a company or bring potential partners or employees to the herd.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      heading: 'How to choose a good Business Plan',
+      text: "Depending on your stage of growth and what you intend to use, business plans can come in a number of different forms.\n\nLet's take a quick look at a few of the most common examples.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      heading: '1. General Business Plan',
+      text: "If your goal is to convince investors to financially support your business, a general business plan - or \"external business plan\", as it is sometimes called - is the most frequently requested response you will need.\n\nGeneral business plans are very well set up and focus on showing investors how your vision translates into greater profits compared to the domestic business.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      heading: '2. One-Page Business Plan',
+      text: "A one-page business plan is actually an executive summary - in other words, the TL; DR version of your business plan where you break down every major part of your business plan into a phase or two, giving investors the opportunity to step in. - see key points.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      text: "One pager is a great help to create early conversations with a potential investor. Investors are incredibly busy, so one pager is a great way to go when you are trying to arouse interest and set up a forum for in-depth discussions about your business after your initial contact.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'textimg',
+    },
+    {
+      heading: '3. Internal Business Plan',
+      text: "As its name implies, internal business plans often reside within the office and are designed to serve as a management tool to help business owners plan and meet goals.\n\nInternal business plans are less concerned with covering issues such as overall team vision or problem solving and are more focused on business strategies, which is the next milestone, budget, and forecast. This type of business plan is more commonly used by companies that are more established than startups.",
+      image: 'https://firebasestorage.googleapis.com/v0/b/reverr-25fb3.appspot.com/o/courses%2FBusinessPlanning%2FBusiness%20Plan.png?alt=media&token=9fb2c48f-6d67-4ce6-b9c0-003485b70694',
+      type: 'headtextimg',
+    },
+    {
+      question: 'Which of the following best describes a one-pager business plan?',
+      options: [
+        'plans are very well set up and focus on showing investors how your vision translates into greater profits compared to the domestic business.',
+        'plans often reside within the office and are designed to serve as a management tool to help business owners plan and meet goals.',
+        'is a great way to go when you are trying to arouse interest and set up a forum for in-depth discussions about your business after your initial contact',
+        'Both A and c',
+      ],
+      ans: 1,
+      type: 'quiz',
+    },
+    ]
+   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ],
+};
+
